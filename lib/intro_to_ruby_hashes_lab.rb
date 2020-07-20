2f55a73f1547562555b793a3f2abb14f05ad2829
Sayana <nisayana23@yandex.ru>
2020-07-20 01:40:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -10,9 +10,11 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  num = {:id => 4}
 end
